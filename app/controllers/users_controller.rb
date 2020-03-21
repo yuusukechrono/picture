3cf04593732efd47e4312de10f8aa88ac1ce0476
yusuke nakano <yuusukechrono@gmail.com>
2020-03-21 16:56:52 +0000

    ユーザー編集

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   skip_before_action :login_required
+  before_action :set_user, only: [:show, :edit, :update]
 
   def new
     @user = User.new
@@ -16,12 +17,26 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find(params[:id])
+  end
+
+  def edit
+  end
+  
+  def update
+    if @user.update(user_params)
+      redirect_to user_path, notice: "編集しました！"
+    else
+      render :edit
+    end
   end
 
   private
 
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation, :image, :image_cache)
+  end
+
+  def set_user
+    @user = User.find(params[:id])     
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,6 @@ Rails.application.routes.draw do
    end
   end
 
-  resources :users, only: [:new, :create, :show]
+  resources :users, only: [:new, :create, :show, :edit, :update]
   resources :sessions, only: [:new, :create, :destroy]
 end
