f150a5af610a616a660e3c24afd6ab60336432a0
yusuke nakano <yuusukechrono@gmail.com>
2020-03-27 18:17:38 +0000

    本番環境でのメール修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -43,7 +43,7 @@ Rails.application.configure do
     user_name: ENV['SENDGRID_USERNAME'],
     password: ENV['SENDGRID_PASSWORD'],
     domain: "heroku.com",
-    address: "smtp.SendGrid.net",
+    address: "smtp.sendgrid.net",
     port: 587,
     authentication: :plain,
     enable_starttls_auto: true
