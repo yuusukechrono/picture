af82b19948bb9c1daede58c8b6f09702f2c26a9b
yusuke nakano <yuusukechrono@gmail.com>
2020-03-31 02:59:37 +0000

    ユーザー編集権限を見直し

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,7 +20,11 @@ class UsersController < ApplicationController
   end
 
   def edit
-      @user = User.find(current_user.id)
+      if User.find(params[:id]) == current_user
+        @user = User.find(current_user.id)
+      else
+        redirect_to pictures_path, notic:"人のIDは編集できません"
+      end
   end
   
   def update
